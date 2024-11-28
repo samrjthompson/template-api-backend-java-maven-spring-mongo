b43263b968c10bd4439bbec37df860f2779caa37
Sam Thompson <66477261+samrjthompson@users.noreply.github.com>
2024-11-28 15:26:14 +0000

    Refactor

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,23 +1,30 @@
 package org.example.config;
 
+import org.example.security.RequestValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final RequestValidationFilter requestValidationFilter;
     private final AuthenticationProvider customAuthenticationProvider;
 
-    public SecurityConfig(AuthenticationProvider customAuthenticationProvider) {
+    public SecurityConfig(RequestValidationFilter requestValidationFilter,
+                          AuthenticationProvider customAuthenticationProvider) {
+        this.requestValidationFilter = requestValidationFilter;
         this.customAuthenticationProvider = customAuthenticationProvider;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(requestValidationFilter, UsernamePasswordAuthenticationFilter.class);
+
         http.httpBasic(Customizer.withDefaults());
         http.authenticationProvider(customAuthenticationProvider);
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
--- src/main/java/org/example/security/InMongoUserDetailsService.java
+++ src/main/java/org/example/security/InMongoUserDetailsService.java
@@ -1,6 +1,10 @@
 package org.example.security;
 
+import static org.example.Main.NAMESPACE;
+
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +12,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class InMongoUserDetailsService implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
+    private static final String USERNAME_NOT_FOUND = "Could not find username: [%s]";
 
     private final MongoTemplate mongoTemplate;
 
@@ -18,6 +25,10 @@ public class InMongoUserDetailsService implements UserDetailsService {
     @Override
     public User loadUserByUsername(final String username) throws UsernameNotFoundException {
         return Optional.ofNullable(mongoTemplate.findById(username, User.class))
-                .orElseThrow(() -> new UsernameNotFoundException(username));
+                .orElseGet(() -> {
+                    final String msg = USERNAME_NOT_FOUND.formatted(username);
+                    LOGGER.error(msg);
+                    throw new UsernameNotFoundException(msg);
+                });
     }
 }
