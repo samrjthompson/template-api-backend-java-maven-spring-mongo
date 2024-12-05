be390bfdeee63856da1e2d26157fddc61b59ede4
Sam <samthompson73@live.co.uk>
2024-12-05 19:28:26 +0000

    Get server working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
-import org.example.config.AppProperties;
+import java.security.NoSuchAlgorithmException;
+import org.example.oauth2.CodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
 public class Main {
 
     public static final String NAMESPACE = "my-application";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         SpringApplication.run(Main.class, args);
+
+        CodeGenerator.generate();
     }
 }
